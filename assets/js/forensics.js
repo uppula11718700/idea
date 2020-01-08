fdf2dcec04e5d9b964e13ce06f046794dc0a4f61
uppula11718700 <36113759+uppula11718700@users.noreply.github.com>
2020-01-08 08:13:57 +0530

    v1

---
+++ assets/js/forensics.js
@@ -0,0 +1,40 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:2,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+});
+$('.owl-carousel-two').owlCarousel({
+    loop:true,
+    margin:2,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+});
---
+++ assets/js/home.js
@@ -0,0 +1,42 @@
+new WOW().init();
+// owl-carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:2,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+});
+$('.owl-carousel-products').owlCarousel({
+    loop:true,
+    margin:2,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+});
